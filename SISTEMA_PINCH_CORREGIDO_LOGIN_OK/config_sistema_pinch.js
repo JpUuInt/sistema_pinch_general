c1a21112d9731ca6a29faaef654c9df800197f04
JpUuInt <166968906+JpUuInt@users.noreply.github.com>
2026-05-21 15:44:29 -0500

    Add files via upload

---
+++ SISTEMA_PINCH_CORREGIDO_LOGIN_OK/config_sistema_pinch.js
@@ -0,0 +1,5 @@
+window.SYSTEM_PINCH_CONFIG = {
+  APP_NAME: 'Sistema Pinch',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyONncYccbdW9hiAfIVFOuC6NCV0RRwLiUZDfrDsnuffY19lruFWdFnHZB9XxWvGdb-fA/exec',
+  SUPPORT_TEXT: 'Configure una sola vez esta URL en config_sistema_pinch.js para usar Campo y Técnico.'
+};
